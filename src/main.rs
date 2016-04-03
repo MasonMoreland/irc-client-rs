ad403fc16866817d2ae8375a04943b5952eb243d
Mason Moreland <MasonMoreland@users.noreply.github.com>
2016-04-02 23:20:37 -0500

    Added config as json

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,22 @@ extern crate irc;
 
 use std::env;
 use irc::client::prelude::*;
+use std::path::Path;
 
 fn main() {
     let mut args = env::args().take(3);
-    let (_, server, nickname) =
-        (args.next().unwrap(),
-        args.next().unwrap(),
-        args.next().unwrap());
 
-    let irc_config = Config {
-        nickname: Some(String::from(nickname)),
-        server: Some(String::from(server)),
-        channels: Some(vec![String::from("#GtAixzOf")]),
-        .. Default::default()
-    };
+    // TODO: fix relative pathing to the config
+    let config_path = Path::new("../config/config.json");
+
+    let irc_config = Config::load(&config_path).unwrap();
 
     let irc_server = IrcServer::from_config(irc_config).unwrap();
     irc_server.identify().unwrap();
 
     loop {
-        irc_server.iter().map(|m| print!("{}",
-                                         m.unwrap())).count();
+        irc_server.iter()
+                  .map(|m| print!("{}", m.unwrap()))
+                  .count();
     }
 }
