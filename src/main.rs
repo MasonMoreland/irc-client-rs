605827902f6547a359d4d765dfb3f90e7aac1dcb
Mason Moreland <MasonMoreland@users.noreply.github.com>
2016-04-02 20:37:30 -0500

    Switched to Andrew's main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 extern crate irc;
 
+use std::env;
 use irc::client::prelude::*;
 
 fn main() {
-        let server = IrcServer::new("config.json").unwrap();
-            server.identify().unwrap();
-                for message in server.iter() {
-                            
-                        }
+    let mut args = env::args().take(3);
+    let (_, server, nickname) =
+        (args.next().unwrap(),
+        args.next().unwrap(),
+        args.next().unwrap());
+
+    let irc_config = Config {
+        nickname: Some(String::from(nickname)),
+        server: Some(String::from(server)),
+        channels: Some(vec![String::from("#GtAixzOf")]),
+        .. Default::default()
+    };
+
+    let irc_server = IrcServer::from_config(irc_config).unwrap();
+    irc_server.identify().unwrap();
+
+    loop {
+        irc_server.iter().map(|m| print!("{}",
+                                         m.unwrap())).count();
+    }
 }
