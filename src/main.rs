ea3339cc4938568127607bfea71e86f8be5f3dd7
Mason Moreland <MasonMoreland@users.noreply.github.com>
2016-04-02 23:31:02 -0500

    Removed unused lines in main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate irc;
 
-use std::env;
+// use std::env;
 use irc::client::prelude::*;
 use std::path::Path;
 
 fn main() {
-    let mut args = env::args().take(3);
+    // let mut args = env::args().take(3);
 
     // TODO: fix relative pathing to the config
     let config_path = Path::new("../config/config.json");
